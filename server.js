08fa87263d7b486b3641fccab92b830050ad5746
Mark Krakof <zoomdeluxe@gmail.com>
2015-09-22 16:15:17 +0300

    status to get

--- server.js
+++ server.js
@@ -11,68 +11,77 @@ http.createServer(function (req, res) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var blogs = db.collection("Blogs");
-        blogs.insertMany( [{
-            domain: "domain1.com/page",
-            originDomain: "domain1.com",
-            engine: "WP",
-            pr: 2,
-            trustflow: 356,
-            keywords: "key1 key2 key3",
-            metaKeywords: "metakey1 metakey2 metakey3",
-            status: "new"
-        },
-            {
-                domain: "domain2.com/page",
-                originDomain: "domain2.com",
-                engine: "JM",
-                pr: 4,
-                trustflow: 16,
-                keywords: "key1 key2 key3",
-                metaKeywords: "metakey1 metakey2 metakey3",
-                status: "done"
-            },
-            {
-                domain: "domain3.com/page",
-                originDomain: "domain3.com",
-                engine: "Presta",
-                pr: 5,
-                trustflow: 625,
-                keywords: "key1 key2 key3",
-                metaKeywords: "metakey1 metakey2 metakey3",
-                status: "queued",
-                queuedTime: new Date()
-            },
-            {
-                domain: "domain4.com/page",
-                originDomain: "domain4.com",
-                engine: "WP",
-                pr: 0,
-                trustflow: 356,
-                keywords: "key1 key2 key3",
-                metaKeywords: "metakey1 metakey2 metakey3",
-                status: "processing"
-            },
-            {
-                domain: "domain5.com/page",
-                originDomain: "domain5.com",
-                engine: "JM",
-                pr: 6,
-                trustflow: 35,
-                keywords: "key1 key2 key3",
-                metaKeywords: "metakey1 metakey2 metakey3",
-                status: "queued",
-                queuedTime: "16:52"
-            }],function (err, results){
-            if (err) throw err;
-            console.log(arguments);
-            db.close();
-        });
+        //blogs.insert( {
+        //    domain: "domain6.com/page",
+        //    originDomain: "domain6.com",
+        //    engine: "Presta",
+        //    pr: 8,
+        //    trustflow: 356,
+        //    keywords: "key1 key2 key3",
+        //    metaKeywords: "metakey1 metakey2 metakey3",
+        //    status: "queued",
+        //    queuedTime: new Date()
+        //},function(err, results){
+        //    if (err) throw err;
+        //    {
+        //        domain: "domain2.com/page",
+        //        originDomain: "domain2.com",
+        //        engine: "JM",
+        //        pr: 4,
+        //        trustflow: 16,
+        //        keywords: "key1 key2 key3",
+        //        metaKeywords: "metakey1 metakey2 metakey3",
+        //        status: "done"
+        //    },
+        //    {
+        //        domain: "domain3.com/page",
+        //        originDomain: "domain3.com",
+        //        engine: "Presta",
+        //        pr: 5,
+        //        trustflow: 625,
+        //        keywords: "key1 key2 key3",
+        //        metaKeywords: "metakey1 metakey2 metakey3",
+        //        status: "queued",
+        //        queuedTime: new Date()
+        //    },
+        //    {
+        //        domain: "domain4.com/page",
+        //        originDomain: "domain4.com",
+        //        engine: "WP",
+        //        pr: 0,
+        //        trustflow: 356,
+        //        keywords: "key1 key2 key3",
+        //        metaKeywords: "metakey1 metakey2 metakey3",
+        //        status: "processing"
+        //    },
+        //    {
+        //        domain: "domain5.com/page",
+        //        originDomain: "domain5.com",
+        //        engine: "JM",
+        //        pr: 6,
+        //        trustflow: 35,
+        //        keywords: "key1 key2 key3",
+        //        metaKeywords: "metakey1 metakey2 metakey3",
+        //        status: "queued",
+        //        queuedTime: new Date()
+        //    }]
+        var halfHour = new Date(new Date() - new Date(30*60000));
+        console.log(halfHour);
+        var json ={};
+        blogs.find({$or: [{status: "new"},{$and: [{status:"queued"}, {queuedTime: {$lt: halfHour}}]}]})
+            .toArray(function (err,results) {
+                //console.dir(results);
+                json = JSON.stringify(results);
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(json);
+                db.close();
+            });
+        //});
+
         console.log("Connected correctly to server");
 
-
     });
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end("Hello World");
+
 
 }).listen(2000);
 console.log('Server running at http://127.0.0.1:2000/');
