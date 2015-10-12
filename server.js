37972d327e6c69dd00237d4b5dcf0a497b0571fc
Mark Krakof <zoomdeluxe@gmail.com>
2015-10-12 10:40:41 +0300

    fara gunoi

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 var http = require('http');
 var url = require("url");
-//var async = require("async");
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var Promise = require('promise');
 var monk = require('monk');
-//var urlDb = 'mongodb://localhost:27017/crawler';
 var db= monk('localhost:27017/crawler');
-//MongoClient.connect(urlDb, function(err, database) {
-//    if (err) throw err;
-//    db = database;
-//    console.log("Connected correctly to server");
-//});
 
 http.createServer(function (req, res) {
         var blogs = db.get("Blogs");
@@ -31,7 +24,6 @@ http.createServer(function (req, res) {
             status: ["required", "string"]
         };
         var dataToSave = [];
-        var countDocs=[];
         function Validator () {
             this["requiredValidate"] = function (el) {
                 var value = (el)||(el === 0)? true:false;
@@ -47,17 +39,6 @@ http.createServer(function (req, res) {
                    var value = (typeof el === "number");
                    return objWrap(value,"number")
             };
-
-            //this["existValidate"] = function(el){
-            //
-            //            return blogs.count({_id: ObjectId(el)}).then(function(count){
-            //                var value = count>0;
-            //                return objWrap(el,value,"exist");
-            //            });
-            //
-            //            //countDocs.push(count>0);
-            //
-            //};
             function objWrap(value,name) {
                 var obj = {};
                 obj[name] = value;
@@ -79,26 +60,7 @@ http.createServer(function (req, res) {
                         }
                     }
                 }
-                
                 return errArr;
-                //Promise.all(isTrue).then(function(results){
-                //    console.log(results);
-                //    var errArr = [];
-                //    for (var t=0; t<results.length; t++) {
-                //        for (var k in results[t]) {
-                //            if (!results[t][k]) {
-                //                errArr.push(results[t]);
-                //            }
-                //        }
-                //    }
-                //    //console.log(errArr);
-                //    if (errArr.length > 0) {
-                //        callback(false,errArr);
-                //    } else {
-                //        //console.log(k);
-                //        callback(true,num);
-                //    }
-                //});
             };
         }
 
@@ -110,7 +72,6 @@ http.createServer(function (req, res) {
             case 'GET':
                 blogs.find({$or: [{status: "new"},{$and: [{status:"queued"}, {queuedTime: {$lt: halfHour}}]}]})
                     .toArray(function (err,results) {
-                        //console.dir(results);
                         jsonGet = JSON.stringify(results);
                         res.writeHead(200, {"Content-Type": "application/json"});
                         res.end(jsonGet);
@@ -144,80 +105,13 @@ http.createServer(function (req, res) {
                         uptadeArr.push(tempObj);
                     }
                     for (var a=0;a<uptadeArr.length; a++){
+                        console.log(uptadeArr[a]._id);
                             blogs.update(uptadeArr[a]._id, uptadeArr[a].params, function (err, result) {
                             console.log(result);
                             console.log(err);
                         });
                     }
-
-                    //console.log(jsonPut);
                 });
         }
-
-        //blogs.insert( {
-        //    domain: "domain6.com/page",
-        //    originDomain: "domain6.com",
-        //    engine: "Presta",
-        //    pr: 8,
-        //    trustflow: 356,
-        //    keywords: "key1 key2 key3",
-        //    metaKeywords: "metakey1 metakey2 metakey3",
-        //    status: "queued",
-        //    queuedTime: new Date()
-        //},function(err, results){
-        //    if (err) throw err;
-        //    {
-        //        domain: "domain2.com/page",
-        //        originDomain: "domain2.com",
-        //        engine: "JM",
-        //        pr: 4,
-        //        trustflow: 16,
-        //        keywords: "key1 key2 key3",
-        //        metaKeywords: "metakey1 metakey2 metakey3",
-        //        status: "done"
-        //    },
-        //    {
-        //        domain: "domain3.com/page",
-        //        originDomain: "domain3.com",
-        //        engine: "Presta",
-        //        pr: 5,
-        //        trustflow: 625,
-        //        keywords: "key1 key2 key3",
-        //        metaKeywords: "metakey1 metakey2 metakey3",
-        //        status: "queued",
-        //        queuedTime: new Date()
-        //    },
-        //    {
-        //        domain: "domain4.com/page",
-        //        originDomain: "domain4.com",
-        //        engine: "WP",
-        //        pr: 0,
-        //        trustflow: 356,
-        //        keywords: "key1 key2 key3",
-        //        metaKeywords: "metakey1 metakey2 metakey3",
-        //        status: "processing"
-        //    },
-        //    {
-        //        domain: "domain5.com/page",
-        //        originDomain: "domain5.com",
-        //        engine: "JM",
-        //        pr: 6,
-        //        trustflow: 35,
-        //        keywords: "key1 key2 key3",
-        //        metaKeywords: "metakey1 metakey2 metakey3",
-        //        status: "queued",
-        //        queuedTime: new Date()
-        //    }]
-
-
-        //if (!Object.keys(queryAsObject).length) console.log("Empty obj");
-        //});
-
-
-        //db.close();
-
-    //});
-
-
 }).listen(2000);
 console.log('Server running at http://127.0.0.1:2000/');
