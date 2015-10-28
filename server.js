8436947c5cc2691954d7436304895ef91d016200
Mark Krakof <zoomdeluxe@gmail.com>
2015-10-28 15:34:14 +0200

    reset

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var url = require("url");
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
+var fs = require('fs');
 var ObjectId = require('mongodb').ObjectID;
 var urlDb = 'mongodb://localhost:27017/crawler';
 MongoClient.connect(urlDb, function(err, database) {
@@ -56,8 +57,7 @@ http.createServer(function (req, res) {
                 return [obj, value];
             }
             function stringEl (elem) {
-                var value = (typeof elem === "string");
-                return value;
+                return (typeof elem === "string");
             }
             this.validate = function(elem) {
                 var tempErr;
@@ -80,49 +80,120 @@ http.createServer(function (req, res) {
 
         switch(req.method) {
             case 'GET':
-                var n= parseInt(url.parse(req.url,true).query.n);
-                var count = (n)?n:10;
-                blogs.find({$or: [{status: "new"},{$and: [{status:"queued"}, {queuedTime: {$lt: halfHour}}]}]}).limit(count)
-                    .toArray(function (err,results) {
-                        jsonGet = JSON.stringify(results);
-                        res.writeHead(200, {"Content-Type": "application/json"});
-                        res.end(jsonGet);
-                        console.log("GET");
-                        if (results.length>0) {
-                           for (var c = 0; c < results.length; c++) {
-                               blogs.update({"_id": ObjectId(results[c]._id)}, {
-                                   $set: {
-                                       status: "queued",
-                                       queuedTime: new Date()
-                                   }
-                               }, {fullresult: true}, function (err, result) {
-                                   if (err) {
-                                       console.log(err.message);
-                                   }
-                               });
-                           }
-                        }
-                    });
+                console.log("GET");
+                console.log(req.url);
+                switch (req.url) {
+                    case '/favicon.ico':
+                        res.writeHead(200, {'Content-Type': 'image/x-icon'});
+                        res.end();
+                        break;
+                    case "/reset?":
+                        blogs.deleteMany({},function(err, results) {
+                            if (err) throw err;
+                            console.log(results.deletedCount);
+                            var sitesArr = [];
+                            var counter = 0;
+                            console.log(results.deletedCount);
+                            var rl = require('readline').createInterface({
+                                input: require('fs').createReadStream('sites.txt')
+                            });
+                            rl.on('line', function (line) {
+                                var objDB = {};
+                                objDB.domain = line.trim();
+                                objDB.status = "new";
+                                sitesArr.push(objDB);
+                                counter++;
+                                if (counter === 20000) {
+                                    blogs.insertMany(sitesArr, function (err, r) {
+                                        if (err) throw err;
+                                    });
+                                    sitesArr = [];
+                                    counter = 0;
+                                }
+                            });
+                            rl.on('close', function(){
+                                blogs.insertMany(sitesArr, function (err, r) {
+                                    if (err) throw err;
+                                    console.log(r.insertedCount);
+                                    res.writeHead(200, {'Content-Type': 'image/x-icon'});
+                                    res.end("Insert DB Done");
+                                });
+                            });
+                        });
+                        break;
+                    case "/stats":
+                        var statObj = {};
+                        var statusCount = [];
+                                blogs.count({status:"new"},function (err, result) {
+                                    statObj["new"]=result;
+                                    blogs.count({status:"queued"},function (err, result) {
+                                        statObj["queued"]=result;
+                                        blogs.count({status:"done"},function (err, result) {
+                                            statObj["done"]=result;
+                                            statusCount.push(statObj);
+                                            statusCount = JSON.stringify(statusCount);
+                                            console.log(statusCount);
+                                            res.writeHead(200, {"Content-Type": "application/json"});
+                                            res.end(statusCount);
+                                            });
+                                        });
+                                    });
+                        break;
+                    default:
+                        var n = parseInt(url.parse(req.url, true).query.n);
+                        var count = (n) ? n : 10;
+                        blogs.find({$or: [{status: "new"}, {$and: [{status: "queued"}, {queuedTime: {$lt: halfHour}}]}]}).limit(count)
+                            .toArray(function (err, results) {
+                                console.log("Sent: " + results.length);
+                                jsonGet = JSON.stringify(results);
+                                res.writeHead(200, {"Content-Type": "application/json"});
+                                res.end(jsonGet);
+                                if (results.length > 0) {
+                                    for (var c = 0; c < results.length; c++) {
+                                        blogs.updateOne({"_id": ObjectId(results[c]._id)}, {
+                                            $set: {
+                                                status: "queued",
+                                                queuedTime: new Date()
+                                            }
+                                        }, {fullresult: true}, function (err, result) {
+                                            if (err) {
+                                                console.log(err.message);
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                }
                 break;
             case 'PUT':
+                var data="";
+                req.on('data', function(chunk){
+                    data+=chunk;
+                });
                 console.log("PUT");
-                req.on('data', function (chunk) {
+                req.on('end', function () {
                     try {
-                        jsonPut = JSON.parse(chunk);
+                        jsonPut = JSON.parse(data);
                     } catch (err) {
-                        console.log("Invalid json");
+                        console.log(err);
+                        fs.writeFile("text.json", data, function(err) {
+                            if(err) {
+                                return console.log(err);
+                            }
+                            console.log("The errlog file was saved!");
+                        }); 
                         res.writeHead(422, {"Content-Type": "application/json"});
                         res.end(err.name);
                         return;
                     }
+                    console.log("Update to validate: " + jsonPut.length);
                     var valid = new Validator();
                     var docErrArr;
                     var uptadeArr = [];
-
+                    console.time("validate");
                     for(var j=0; j<jsonPut.length; j++){
                         var tempObj = {};
                         docErrArr = valid.validate(jsonPut[j]);
-                        console.log(jsonPut[j]);
                         if (docErrArr.length >0) {
                             docErrArr = JSON.stringify(docErrArr);
                             console.log(docErrArr);
@@ -143,22 +214,24 @@ http.createServer(function (req, res) {
                         }
                         uptadeArr.push(tempObj);
                     }
-                    console.log(uptadeArr.length);
+                    console.timeEnd("validate");
+                    console.log("For DB Update: " + uptadeArr.length);
                     for (var a=0;a<uptadeArr.length; a++){
-                            blogs.update({"_id":ObjectId(uptadeArr[a]._id)}, {$set:uptadeArr[a].params}, {fullResult: true},function (err,r) {
+                            blogs.updateOne({"_id":ObjectId(uptadeArr[a]._id)}, {$set:uptadeArr[a].params}, {fullResult: true},function (err,r) {
                             if (err){
                                 console.log("DB update Error");
                                 res.writeHead(422, {"Content-Type": "application/json"});
                                 res.end(JSON.stringify(err.message));
                                 return;
                             } else {
-                                console.log(r.result.n);
+                                //console.log(r.result.n);
                             }
                         });
                     }
                     res.writeHead(200, {"Content-Type": "application/json"});
-                    res.end("Input done");
+                    res.end("Input of " + uptadeArr.length + " documents: done");
                 });
+                break;
         }
 }).listen(2000);
 console.log('Server running at http://127.0.0.1:2000/');
